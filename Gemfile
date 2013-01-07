bb18bdb5daea3650693403ba048940190f7b7c04
ersk <erik.skoglund@hb.se>
2013-01-07 09:49:37 +0100

    init commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.10'
+
+group :development do
+  gem 'sqlite3'
+end
+
+gem 'bluecloth'
+
+group :production do
+  gem 'pg'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'bootstrap-sass'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Markitdown::Application.config.secret_token = '1f8f7a78bb5095ebbeb1f9223590fde8de79b9eb5383a6b82bfab5cb8098fa75dd3cd68d384a52306008a56d9966af98dc5a0cdc01da69b99da723c083db73f5'
